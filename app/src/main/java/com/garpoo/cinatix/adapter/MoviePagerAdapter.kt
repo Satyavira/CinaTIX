7ab8d7d597d3b46150a9f00db24eba2a07672da6
Satyavira <viraprathama@gmail.com>
2024-11-06 23:04:26 +0700

    Create MoviePagerAdapter to be use for viewpager adapter and inside some notable thing I use is glide, handler, and inside the glide i also use requestoption that is use to crop the image and make it to have rounded corner)

---
+++ app/src/main/java/com/garpoo/cinatix/adapter/MoviePagerAdapter.kt
@@ -0,0 +1,85 @@
+package com.garpoo.cinatix.adapter
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.garpoo.cinatix.databinding.ItemMoviePosterBinding
+import com.garpoo.cinatix.model.Movie
+
+class MoviePagerAdapter(
+    private var movies: MutableList<Movie>,
+    private val viewPager2: ViewPager2
+) : RecyclerView.Adapter<MoviePagerAdapter.MovieViewHolder>() {
+
+    private var context: Context? = null
+
+    // Runnable to duplicate items for infinite scrolling
+    private val runnable = Runnable {
+        movies.addAll(movies) // Duplicate items for infinite scrolling
+        notifyDataSetChanged() // Refresh adapter to display new items
+    }
+
+    // ViewHolder class with ViewBinding for `item_movie_poster`
+    inner class MovieViewHolder(private val binding: ItemMoviePosterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        // Bind method to load data into views
+        fun bind(movie: Movie) {
+            context?.let {
+                Glide.with(it)
+                    .load(movie.posterUrl)
+                    .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(60))) // Center crop with rounded corners
+                    .into(binding.moviePosterImage) // Load image into ImageView
+            }
+
+            // Set text data
+            binding.movieTitle.text = movie.title
+            binding.movieGenre.text = movie.genre
+
+            // Show/hide movie information based on the current position
+            if (adapterPosition == currentPosition) {
+                binding.movieInfoLayout.visibility = View.VISIBLE
+            } else {
+                binding.movieInfoLayout.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    // Track the current position for infinite scrolling effect
+    private var currentPosition = 0
+
+    fun setCurrentPosition(position: Int) {
+        currentPosition = position
+        Handler(Looper.getMainLooper()).post {
+            notifyItemChanged(position - 1)
+            notifyItemChanged(position)
+            notifyItemChanged(position + 1)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        context = parent.context
+        val binding = ItemMoviePosterBinding.inflate(LayoutInflater.from(context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+
+        // Trigger the runnable for infinite scrolling when reaching the second-last item
+//        if (position == movies.size - 2) {
+//            viewPager2.post(runnable)
+//        }
+    }
+
+    override fun getItemCount(): Int = movies.size
+}
