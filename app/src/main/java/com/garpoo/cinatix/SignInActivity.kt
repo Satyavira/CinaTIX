eb64e5fa28c9f5ef820d83bcfbc3212de3c4e479
DP6-Fico-098 <sibagariangfico@gmail.com>
2024-11-15 14:08:15 +0700

    menjalankan sign in dan sign up dan sudah terhubung ke database firebase

--- app/src/main/java/com/garpoo/cinatix/SignInActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/SignInActivity.kt
@@ -1,20 +1,45 @@
 package com.garpoo.cinatix
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.garpoo.cinatix.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signupButton.setOnClickListener {
+            val email = binding.signinEmail.text.toString()
+            val password = binding.signinPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.txtDaftar.setOnClickListener {
+            val signupIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signupIntent)
         }
     }
 }
--- app/src/main/java/com/garpoo/cinatix/SignUpActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/SignUpActivity.kt
@@ -1,20 +1,57 @@
 package com.garpoo.cinatix
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.garpoo.cinatix.databinding.ActivitySignUpBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var email: TextInputEditText
+    private lateinit var new_password: TextInputEditText
+    private lateinit var retype_password: TextInputEditText
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener{
+            val email = binding.yourEmail.text.toString()
+            val password = binding.newPassword.text.toString()
+            val retype = binding.retypePassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && retype.isNotEmpty()){
+                if (password == retype){
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password does not matched", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
+
 }
