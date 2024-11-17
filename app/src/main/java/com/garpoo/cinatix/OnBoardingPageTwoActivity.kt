2c54d6237cffd2d5199a576a09dcf1f0b9333f84
Satyavira <viraprathama@gmail.com>
2024-11-17 22:32:02 +0700

    Modified OnBoardingPageTwoActivity.kt. Remove the padding insets. Add binding variable. Set onclick listener for next button and skip button.

--- app/src/main/java/com/garpoo/cinatix/OnBoardingPageTwoActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/OnBoardingPageTwoActivity.kt
@@ -1,20 +1,25 @@
 package com.garpoo.cinatix
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.garpoo.cinatix.databinding.ActivityOnBoardingPageTwoBinding
 
 class OnBoardingPageTwoActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityOnBoardingPageTwoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_page_two)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, OnboardPageThreeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.skipButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
         }
     }
 }
