7fdb2f2ceaa0e8cf3fd4a9712ee3a51fdd380898
Satyavira <viraprathama@gmail.com>
2024-11-19 23:11:54 +0700

    Modified MovieItemRecyclerAdapter.kt. Add a function as a parameter that will be use for view holder to be click to go to another activity that is sipnosis.

--- app/src/main/java/com/garpoo/cinatix/adapter/MovieItemRecyclerAdapter.kt
+++ app/src/main/java/com/garpoo/cinatix/adapter/MovieItemRecyclerAdapter.kt
@@ -18,7 +18,8 @@ import java.text.DecimalFormat
 import kotlin.math.nextUp
 
 class MovieItemRecyclerAdapter(
-    private var movies: List<Movie>
+    private var movies: List<Movie>,
+    private val movieToSipnosis: (Int) -> Unit
 ) : RecyclerView.Adapter<MovieItemRecyclerAdapter.MovieViewHolder>() {
 
     private var context: Context? = null
@@ -35,6 +36,9 @@ class MovieItemRecyclerAdapter(
 
         // Bind method to load data into views
         fun bind(movie: Movie) {
+            binding.root.setOnClickListener {
+                movieToSipnosis.invoke(movie.id)
+            }
             context?.let {
                 // Load movie poster using Glide with rounded corners
                 Glide.with(it)
