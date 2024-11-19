4c6a2ce43c45e65c88a4eb2a5fe71b3f9167305d
Satyavira <viraprathama@gmail.com>
2024-11-19 12:53:27 +0700

    Modified WelcomeActivity.kt. Add binding and create intent from WelcomeActivity to HomeActivity.

--- app/src/main/java/com/garpoo/cinatix/WelcomeActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/WelcomeActivity.kt
@@ -1,20 +1,24 @@
 package com.garpoo.cinatix
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.garpoo.cinatix.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lihatFilmButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
