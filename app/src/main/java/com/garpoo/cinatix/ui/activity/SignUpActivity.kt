024e5265fe9a1db569f19679b3c5afd8fafa4043
Satyavira <viraprathama@gmail.com>
2024-11-24 15:35:10 +0700

    Modified SignUpActivity.kt. Add a new variable that is name that will take the data from the text field and for the condition add name as one of the prerequisite. And modified after creating the account from just go to another activity it become modified the profile name first and after that it goes to another activity.

--- app/src/main/java/com/garpoo/cinatix/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/ui/activity/SignUpActivity.kt
@@ -2,12 +2,14 @@ package com.garpoo.cinatix.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.garpoo.cinatix.databinding.ActivitySignUpBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -26,19 +28,36 @@ class SignUpActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btnRegister.setOnClickListener{
+            val name = binding.yourName.text.toString()
             val email = binding.yourEmail.text.toString()
             val password = binding.newPassword.text.toString()
             val retype = binding.retypePassword.text.toString()
 
-            if (email.isNotEmpty() && password.isNotEmpty() && retype.isNotEmpty()){
+            if (email.isNotEmpty() && password.isNotEmpty() && retype.isNotEmpty() && name.isNotEmpty()){
                 if (password == retype){
 
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                        if (it.isSuccessful){
-                            val intent = Intent(this, SignInActivity::class.java)
-                            startActivity(intent)
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ it1 ->
+                        if (it1.isSuccessful){
+                            firebaseAuth.currentUser?.let {
+                                val profileUpdates = UserProfileChangeRequest.Builder()
+                                    .setDisplayName(name)
+                                    .build()
+
+                                it.updateProfile(profileUpdates)
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            val intent = Intent(this, SignInActivity::class.java)
+                                            startActivity(intent)
+                                            Log.d("Firebase", "User profile updated.")
+                                            finish()
+                                        } else {
+                                            Log.e("Firebase", "Profile update failed", task.exception)
+                                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                            }
                         } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, it1.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
                 } else {
