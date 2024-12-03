a6e9b01fd89a47d192f5b1e9c0cf61efc4ccadce
Satyavira <viraprathama@gmail.com>
2024-12-04 00:02:31 +0700

    Modified HomeActivity.kt. Delete hardcoded list and the other code that is not used.

--- app/src/main/java/com/garpoo/cinatix/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/garpoo/cinatix/ui/activity/HomeActivity.kt
@@ -60,89 +60,6 @@ class HomeActivity : AppCompatActivity() {
 
         sliderHandler = Handler(Looper.getMainLooper())
 
-        // Hardcoded list of movies for now
-        val movies = mutableListOf(
-            Movie(
-                adult = false,
-                backdrop_path = "/gMQibswELoKmB60imE7WFMlCuqY.jpg",
-                genre_ids = listOf(27, 53, 9648),
-                id = 1034541,
-                original_language = "en",
-                original_title = "Terrifier 3",
-                overview = "Five years after surviving Art the Clown's Halloween massacre, Sienna and Jonathan are still struggling to rebuild their shattered lives. As the holiday season approaches, they try to embrace the Christmas spirit and leave the horrors of the past behind. But just when they think they're safe, Art returns, determined to turn their holiday cheer into a new nightmare. The festive season quickly unravels as Art unleashes his twisted brand of terror, proving that no holiday is safe.",
-                popularity = 6883.159,
-                poster_path = "/63xYQj1BwRFielxsBDXvHIJyXVm.jpg",
-                release_date = "2024-10-09",
-                title = "Terrifier 3",
-                video = false,
-                vote_average = 7.3,
-                vote_count = 585
-            ),
-            Movie(
-                adult = false,
-                backdrop_path = "/3V4kLQg0kSqPLctI5ziYWabAZYF.jpg",
-                genre_ids = listOf(878, 28, 12),
-                id = 912649,
-                original_language = "en",
-                original_title = "Venom: The Last Dance",
-                overview = "Eddie and Venom are on the run. Hunted by both of their worlds and with the net closing in, the duo are forced into a devastating decision that will bring the curtains down on Venom and Eddie's last dance.",
-                popularity = 5590.757,
-                poster_path = "/k42Owka8v91trK1qMYwCQCNwJKr.jpg",
-                release_date = "2024-10-22",
-                title = "Venom: The Last Dance",
-                video = false,
-                vote_average = 6.7,
-                vote_count = 467
-            ),
-            Movie(
-                adult = false,
-                backdrop_path = "/4zlOPT9CrtIX05bBIkYxNZsm5zN.jpg",
-                genre_ids = listOf(16, 878, 10751),
-                id = 1184918,
-                original_language = "en",
-                original_title = "The Wild Robot",
-                overview = "After a shipwreck, an intelligent robot called Roz is stranded on an uninhabited island. To survive the harsh environment, Roz bonds with the island's animals and cares for an orphaned baby goose.",
-                popularity = 4321.421,
-                poster_path = "/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg",
-                release_date = "2024-09-12",
-                title = "The Wild Robot",
-                video = false,
-                vote_average = 8.5,
-                vote_count = 2365
-            ),
-            Movie(
-                adult = false,
-                backdrop_path = "/oPUOpnl3pqD8wuidjfUn17mO1yA.jpg",
-                genre_ids = listOf(16, 878, 12, 10751),
-                id = 698687,
-                original_language = "en",
-                original_title = "Transformers One",
-                overview = "The untold origin story of Optimus Prime and Megatron, better known as sworn enemies, but once were friends bonded like brothers who changed the fate of Cybertron forever.",
-                popularity = 2550.704,
-                poster_path = "/iHPIBzrjJHbXeY9y7VVbEVNt7LW.jpg",
-                release_date = "2024-09-11",
-                title = "Transformers One",
-                video = false,
-                vote_average = 8.169,
-                vote_count = 528
-            ),
-            Movie(
-                adult = false,
-                backdrop_path = "/9oYdz5gDoIl8h67e3ccv3OHtmm2.jpg",
-                genre_ids = listOf(18, 27, 878),
-                id = 933260,
-                original_language = "en",
-                original_title = "The Substance",
-                overview = "Have you ever dreamt of a better version of yourself? You, only better in every way. You should try this new product, it's called The Substance. IT CHANGED MY LIFE.",
-                popularity = 2881.789,
-                poster_path = "/lqoMzCcZYEFK729d6qzt349fB4o.jpg",
-                release_date = "2024-09-07",
-                title = "The Substance",
-                video = false,
-                vote_average = 7.3,
-                vote_count = 1359
-            )
-        )
         viewPager2 = binding.viewPager2
         viewPagerIndicator = binding.viewPagerIndicator
         recyclerViewComingSoon = binding.recyclerViewComingSoon
@@ -220,13 +137,6 @@ class HomeActivity : AppCompatActivity() {
         binding.bottomNavigation.selectedItemId = R.id.nav_home
         BottomNavigationHandler.handleNavigation(this@HomeActivity,binding.bottomNavigation)
 
-
-
-        // Firebase code is commented out for now
-        /*
-        database = FirebaseDatabase.getInstance()
-        initBanner()
-        */
     }
 
     override fun onRestart() {
@@ -338,26 +248,4 @@ class HomeActivity : AppCompatActivity() {
         sliderHandler.postDelayed(sliderRunnable, 2000)
     }
 
-    /*
-    private fun initBanner() {
-        val myRef: DatabaseReference = database.getReference("Banners")
-        binding.progressBarSlider.visibility = View.VISIBLE
-
-        myRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val lists = mutableListOf<SliderItems>()
-                for (childSnapshot in snapshot.children) {
-                    val list = childSnapshot.getValue(SliderItems::class.java)
-                    if (list != null) {
-                        lists.add(list)
-                    }
-                }
-                binding.progressBarSlider.visibility = View.GONE
-                setupViewPager(lists)
-            }
-
-            override fun onCancelled(error: DatabaseError) {}
-        })
-    }
-    */
 }
